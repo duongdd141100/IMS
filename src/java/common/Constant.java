2d8af7d4155baffe00782626125c89f1cbc0da3d
Đỗ Đức Dương <83938501+duongdd141100@users.noreply.github.com>
2023-11-02 15:32:20 +0700

    create common

---
+++ src/java/common/Constant.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package common;
+
+/**
+ *
+ * @author Do Duc Duong
+ */
+public class Constant {
+    public static final String CREATE_ACTION = "CREATE";
+    public static final String UPDATE_ACTION = "UPDATE";
+    public static final String DELETE_ACTION = "DELETE";
+}
